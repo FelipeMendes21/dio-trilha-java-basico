edc343cd7faf8f8abd687eb9953b95f80efdaefd
Seu Nome <seu-email@example.com>
2024-10-28 18:59:44 -0300

    Operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        double salarioMinimo = 2500.35;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.print(numero);
+
+       // double VALOR_DE_PI = 3.14;
+        //VALOR_DE_PI = 10.75 
     }
 }
