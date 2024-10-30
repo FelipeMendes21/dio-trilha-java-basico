8fa001a8e04152e782b11e2935dad69346989c05
Seu Nome <seu-email@example.com>
2024-10-29 23:32:34 -0300

    sistema-smart-tv

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,3 +1,16 @@
 public class Operadores {
-    
+
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condicoes sao verdadeiras");
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("uma das condicoes sao verdadeiras");
+        }
+
+        System.out.println("fim");
+    }
 }
