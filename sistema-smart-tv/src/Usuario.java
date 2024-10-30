eb98ef41668d902f96e30a99a3cea43bc2d3d856
Seu Nome <seu-email@example.com>
2024-10-30 00:08:33 -0300

    Sist-smart-tv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,32 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+
+        System.out.println("TV Canal Atual : " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+        
+        System.out.println("TV Canal Atual : " + smartTv.canal);
+
+        System.out.println("TV Volume Atual : " + smartTv.volume);
+ 
+        
+        System.out.println("TV ligada ? " + smartTv.ligada);
+        System.out.println("TV Volume Atual : " + smartTv.volume);
+
+
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+
+        smartTv.desligar ();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+    }
+}
