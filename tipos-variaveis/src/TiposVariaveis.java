77cbc5899eb5b09fc0ea243c58d819161635f5c5
Seu Nome <seu-email@example.com>
2024-10-29 23:12:02 -0300

    Aul de hoje1

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,16 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        double salarioMinimo = 2500.35;
+        //double salarioMinimo = 2500.35;
 
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
+       // short numeroCurto = 1;
+      //  int numeroNormal = numeroCurto;
+       // short numeroCurto2 = (short) numeroNormal;
 
-        int numero = 1;
+       //int numero = 1;
 
-        numero = 2;
+       // numero = 2;
 
-        System.out.print(numero);
+       // System.out.print(numero);
 
        // double VALOR_DE_PI = 3.14;
         //VALOR_DE_PI = 10.75 
