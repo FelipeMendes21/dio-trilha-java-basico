8d4baa15834dda7c522fc8ad7b04c9e1f1be5e36
Felipe Mendes <lipe.mendes200@gmail.com>
2024-11-08 09:33:44 -0300

    Estrtutura de repeticao

---
+++ Estrutura-De-Repeticao/src/ExemploFor.java
@@ -0,0 +1,12 @@
+
+
+public class ExemploFor {
+    public static void main(String[] args) throws Exception {
+        
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+            System.out.print("Contando Carneirinhos " + carneirinhos);
+        }
+
+        System.out.println("joaozinho dormiu");
+    }
+}
---
+++ Estrutura-De-Repeticao/src/ForArray.java
@@ -0,0 +1,10 @@
+public class ForArray {
+    public static void main(String[] args) {
+
+        String alunos[] = { "Felipe", "Julia", "Jonas", "Marcos" };
+        
+        for (String aluno : alunos) {
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
